c9221467e2235b2c2f66d9653dc30e66de1825b6
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-05-17 18:15:28 -0300

    #

--- Modulo_1/TP2_Casco_Alejandro/ejercicio_5.js
+++ Modulo_1/TP2_Casco_Alejandro/ejercicio_5.js
@@ -1,11 +1,16 @@
 
 
-// let texto = "Alejandro"
-let texto = "EMILIO"
-// let texto3 = "florencia"
+let inputString = prompt("Por favor, ingresé un mensaje o palabra a elección:","...");
+
 
 function analizarTexto(texto) {
     if (texto === texto.toUpperCase()){
-        console.log("Este texto está todo en mayúscula");
+        return "todos los caracteres de su string están en mayúscula";
+    } else if (texto === texto.toLowerCase()){
+        return "todos los caracteres de su string están en minúsculas";
+    } else {
+        return "Este texto contiene tanto minúsculas como mayúsculas";
     }
 }
+
+document.write(analizarTexto(inputString));
