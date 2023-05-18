239f537d71cc09b62c864f24c2983eb01db5b7b4
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-05-18 17:14:42 -0300

    #

--- Modulo_1/TP2_Casco_Alejandro/ejercicio_1.js
+++ Modulo_1/TP2_Casco_Alejandro/ejercicio_1.js
@@ -1,9 +1,13 @@
-
+//Consigna: A partir del siguiente array que se proporciona:
 var valores = [true, false, 2, "hola", "mundo", 3, "char"];
 
+// 1. Determinar cuál de los “elementos de texto” es mayor, es decir el que contenga más letras.
+// 2. Imprimir estos elementos de menor a mayor cantidad de letras.
+// 3. Determinar el resultado de las cuatro operaciones matemáticas básicas (suma, resta, multiplicación y división) realizadas con los dos elementos numéricos.
+
 let masLargo = "";
-var nuevoArreglo = [];
-var arregloNumeros = [];
+let nuevoArreglo = [];
+let arregloNumeros = [];
 
 
 for(let i=0; i < valores.length; i++){
