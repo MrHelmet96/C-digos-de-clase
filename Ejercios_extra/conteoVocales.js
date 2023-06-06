c33ded09fe5968dd821b9864c907a3d5453f814e
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-06-05 23:47:06 -0300

    #

--- Ejercios_extra/conteoVocales.js
+++ Ejercios_extra/conteoVocales.js
@@ -3,10 +3,30 @@
 //pedir una frase
 let inputFrase = prompt("Por favor, escriba una frase de al menos 30 caracteres", "...");
 
-let arrayDeVocales = [];
+// Crear un objeto para almacenar las vocales y sus frecuencias
+const vocales = {
+  a: 0,
+  e: 0,
+  i: 0,
+  o: 0,
+  u: 0
+};
 
-if (inputFrase.length < 30) {
-    alert("Su mensaje tiene menos de 30 caracteres, por favor, ")
-} else {
+// Convertir la frase a minúsculas para hacer las comparaciones de forma insensible a mayúsculas
+const inputMinuscula = inputFrase.toLowerCase();
 
+// Recorrer cada caracter de la frase
+for (let i = 0; i < inputMinuscula.length; i++) {
+  const caracter = inputMinuscula[i];
+  
+  // Verificar si el caracter es una vocal
+  if (caracter in vocales) {
+    // Incrementar el contador de la vocal correspondiente
+    vocales[caracter]++;
+  }
+}
+
+// Mostrar las frecuencias de las vocales en la frase
+for (let vocal in vocales) {
+  document.write(`La vocal ${vocal} aparece ${vocales[vocal]} veces<br>`);
 }
