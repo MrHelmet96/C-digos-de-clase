5a1bd4872222ad18d4a0f789b402cac06ea5f8e2
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-06-06 17:32:00 -0300

    #

--- Ejercios_extra/piramide.js
+++ Ejercios_extra/piramide.js
@@ -1,16 +1,12 @@
 // Realiza un script que escriba por pantalla una piramide del 1 al 20 de la siguiente forma: 
-// 1
-// 22
-// 333
-// 4444
-// 55555
+//1
+//22
+//333
+//4444
 
 for (let i = 1; i <= 20; i++) {
-    let linea = "";
-    document.write("</br>")
-    for (let j = 0; j < i; j++) {
-      linea += i;
-    }
-    document.write(linea);
+  for (let repeticion = 0; repeticion < i; repeticion++){
+    document.write(i);
   }
-  
+  document.write("<br>");
+}
